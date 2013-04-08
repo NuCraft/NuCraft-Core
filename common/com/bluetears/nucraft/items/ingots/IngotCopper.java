6cb8096bef28b2a2dc0d6618fca170cdb03285bc
BlueTears <maziarkj@my.erau.edu>
2013-04-08 01:36:44 -0500

    LOL testing ore dict stuff

---
+++ common/com/bluetears/nucraft/items/ingots/IngotCopper.java
@@ -0,0 +1,25 @@
+package com.bluetears.nucraft.items.ingots;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class IngotCopper extends Item
+{
+
+
+public IngotCopper(int id)
+{
+         super(id);
+         setCreativeTab(CreativeTabs.tabMisc);
+         setMaxStackSize(64);
+         setUnlocalizedName("graphite");
+}
+
+@Override
+public void updateIcons(IconRegister iconRegister)
+{
+         iconIndex = iconRegister.registerIcon("nucraft:graphite");
+}
+
+}
